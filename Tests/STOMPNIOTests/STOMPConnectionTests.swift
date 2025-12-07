077ea257be3de7b62c69065bf7e2c6c6e5a8ef2d
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2025-12-07 23:58:06 +0100

    Unix domain sockets

--- Tests/STOMPNIOTests/STOMPConnectionTests.swift
+++ Tests/STOMPNIOTests/STOMPConnectionTests.swift
@@ -16,11 +16,7 @@ struct STOMPConnectionTests {
     func pubSub(ackMode: STOMPAckMode) async throws {
         try await withThrowingTaskGroup(of: Void.self) { group in
             group.addTask {
-                try await STOMPConnection.withConnection(
-                    host: Self.hostname,
-                    port: 61613,
-                    logger: self.subscriberLogger
-                ) { connection in
+                try await STOMPConnection.withConnection(address: .hostname(Self.hostname), logger: self.subscriberLogger) { connection in
                     try await connection.subscribe(to: "/queue/a", ackMode: ackMode) { subscription in
                         for try await frame in subscription {
                             #expect(String(buffer: frame.body) == "Hello, STOMP over NIO!")
@@ -31,11 +27,7 @@ struct STOMPConnectionTests {
             }
 
             group.addTask {
-                try await STOMPConnection.withConnection(
-                    host: Self.hostname,
-                    port: 61613,
-                    logger: self.publisherLogger
-                ) { connection in
+                try await STOMPConnection.withConnection(address: .hostname(Self.hostname), logger: self.publisherLogger) { connection in
                     try await connection.send(ByteBuffer(string: "Hello, STOMP over NIO!"), to: "/queue/a")
                 }
             }
@@ -47,20 +39,15 @@ struct STOMPConnectionTests {
     @Test("Connect with Wrong Host and Port")
     func wrongHostAndPort() async throws {
         await #expect(throws: (any Error).self) {
-            _ = try await STOMPConnection.withConnection(
-                host: "invalid-host",
-                port: 12345,
-                logger: self.logger
-            ) { _ in }
+            try await STOMPConnection.withConnection(address: .hostname("invalid-host", port: 12345), logger: self.logger) { _ in }
         }
     }
 
     @Test("Connect with Wrong Credentials")
     func wrongCredentials() async throws {
         await #expect(throws: STOMPClientError.errorFrame(message: "Bad CONNECT", body: "Access refused for user 'wrong-user'")) {
-            _ = try await STOMPConnection.withConnection(
-                host: Self.hostname,
-                port: 61613,
+            try await STOMPConnection.withConnection(
+                address: .hostname(Self.hostname),
                 configuration: .init(
                     authentication: .init(login: "wrong-user", passcode: "wrong-pass")
                 ),
@@ -71,11 +58,7 @@ struct STOMPConnectionTests {
 
     @Test("Send Frame")
     func sendFrame() async throws {
-        try await STOMPConnection.withConnection(
-            host: Self.hostname,
-            port: 61613,
-            logger: self.logger
-        ) { connection in
+        try await STOMPConnection.withConnection(address: .hostname(Self.hostname), logger: self.logger) { connection in
             let frame = STOMPFrame(
                 command: .send,
                 headers: [
@@ -96,8 +79,7 @@ struct STOMPConnectionTests {
     func connectedTimeout() async throws {
         await #expect(throws: STOMPClientError.timeout) {
             try await STOMPConnection.withConnection(
-                host: Self.hostname,
-                port: 61613,
+                address: .hostname(Self.hostname),
                 configuration: .init(connectTimeout: .milliseconds(1)),
                 logger: self.logger
             ) { _ in }
@@ -107,8 +89,7 @@ struct STOMPConnectionTests {
     @Test("RECEIPT Timeout")
     func receiptTimeout() async throws {
         try await STOMPConnection.withConnection(
-            host: Self.hostname,
-            port: 61613,
+            address: .hostname(Self.hostname),
             configuration: .init(receiptTimeout: .nanoseconds(1)),
             logger: self.logger
         ) { connection in
@@ -123,29 +104,21 @@ struct STOMPConnectionTests {
             )
 
             await #expect(throws: STOMPClientError.timeout) {
-                _ = try await connection.send(frame: frame)
+                try await connection.send(frame: frame)
             }
         }
     }
 
     @Test("Graceful Shutdown")
     func gracefulShutdown() async throws {
-        try await STOMPConnection.withConnection(
-            host: Self.hostname,
-            port: 61613,
-            logger: self.logger
-        ) { connection in
+        try await STOMPConnection.withConnection(address: .hostname(Self.hostname), logger: self.logger) { connection in
             try await connection.triggerGracefulShutdown()
         }
     }
 
     @Test("Send after Graceful Shutdown")
     func sendAfterGracefulShutdown() async throws {
-        try await STOMPConnection.withConnection(
-            host: Self.hostname,
-            port: 61613,
-            logger: self.logger
-        ) { connection in
+        try await STOMPConnection.withConnection(address: .hostname(Self.hostname), logger: self.logger) { connection in
             try await connection.triggerGracefulShutdown()
             await #expect(throws: STOMPClientError.connectionClosed) {
                 try await connection.send(ByteBuffer(string: "Test"), to: "/queue/test")
@@ -157,8 +130,7 @@ struct STOMPConnectionTests {
     @Test("Connect with NIOTransportServices")
     func nioTransportServices() async throws {
         try await STOMPConnection.withConnection(
-            host: Self.hostname,
-            port: 61613,
+            address: .hostname(Self.hostname),
             eventLoop: NIOTSEventLoopGroup.singleton.any(),
             logger: self.logger
         ) { connection in
