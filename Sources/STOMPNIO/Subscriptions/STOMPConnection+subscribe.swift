03217374bd6dae19f9f06ff596f801c7b75de86e
Francesco Paolo Severino <96546612+fpseverino@users.noreply.github.com>
2026-01-03 19:21:29 +0100

    Split `userDefinedHeaders` in `subscribeUserDefinedHeaders` and `unsubscribeUserDefinedHeaders` (#19)
    * Split `userDefinedHeaders` in `subscribeUserDefinedHeaders` and `unsubscribeUserDefinedHeaders`
    
    * Simplify parameter names

--- Sources/STOMPNIO/Subscriptions/STOMPConnection+subscribe.swift
+++ Sources/STOMPNIO/Subscriptions/STOMPConnection+subscribe.swift
@@ -7,17 +7,23 @@ extension STOMPConnection {
     /// - Parameters:
     ///   - destination: The destination to subscribe to
     ///   - ackMode: The acknowledgment mode for the subscription
-    ///   - userDefinedHeaders: Additional headers to include in the `SUBSCRIBE` and `UNSUBSCRIBE` frames
+    ///   - subscribeHeaders: Additional user defined headers to include in the `SUBSCRIBE` frame
+    ///   - unsubscribeHeaders: Additional user defined headers to include in the `UNSUBSCRIBE` frame
     ///   - process: Closure where messages received from the subscription are processed.
     ///     The closure receives a ``STOMPSubscription`` `AsyncSequence` to listen for messages.
     @inlinable
     public nonisolated func subscribe<Value>(
         to destination: String,
         ackMode: STOMPSubscription.AckMode = .auto,
-        userDefinedHeaders: STOMPHeaders = [:],
+        subscribeHeaders: STOMPHeaders = [:],
+        unsubscribeHeaders: STOMPHeaders = [:],
         process: (STOMPSubscription) async throws -> Value
     ) async throws -> Value {
-        let (id, stream) = try await self.subscribe(destination: destination, ackMode: ackMode, userDefinedHeaders: userDefinedHeaders)
+        let (id, stream) = try await self.subscribe(
+            destination: destination,
+            ackMode: ackMode,
+            userDefinedHeaders: subscribeHeaders
+        )
         let value: Value
         do {
             value = try await process(stream)
@@ -25,13 +31,13 @@ extension STOMPConnection {
         } catch {
             // call unsubscribe in unstructured Task to avoid it being cancelled
             _ = await Task {
-                try await self.unsubscribe(id: id, userDefinedHeaders: userDefinedHeaders)
+                try await self.unsubscribe(id: id, userDefinedHeaders: unsubscribeHeaders)
             }.result
             throw error
         }
         // call unsubscribe in unstructured Task to avoid it being cancelled
         _ = try await Task {
-            try await self.unsubscribe(id: id, userDefinedHeaders: userDefinedHeaders)
+            try await self.unsubscribe(id: id, userDefinedHeaders: unsubscribeHeaders)
         }.value
         return value
     }
--- Sources/STOMPNIO/Transactions/STOMPTransaction.swift
+++ Sources/STOMPNIO/Transactions/STOMPTransaction.swift
@@ -59,20 +59,22 @@ public struct STOMPTransaction: Sendable {
     /// - Parameters:
     ///   - destination: The destination to subscribe to
     ///   - ackMode: The acknowledgment mode for the subscription
-    ///   - userDefinedHeaders: Additional headers to include in the `SUBSCRIBE` and `UNSUBSCRIBE` frames
+    ///   - subscribeHeaders: Additional user defined headers to include in the `SUBSCRIBE` frame
+    ///   - unsubscribeHeaders: Additional user defined headers to include in the `UNSUBSCRIBE` frame
     ///   - process: Closure where messages received from the subscription are processed.
     ///     The closure receives a ``STOMPSubscription`` `AsyncSequence` to listen for messages.
     @inlinable
     public nonisolated func subscribe<Value>(
         to destination: String,
         ackMode: STOMPSubscription.AckMode = .auto,
-        userDefinedHeaders: STOMPHeaders = [:],
+        subscribeHeaders: STOMPHeaders = [:],
+        unsubscribeHeaders: STOMPHeaders = [:],
         process: (STOMPSubscription) async throws -> Value
     ) async throws -> Value {
         let (id, stream) = try await self.connection.subscribe(
             destination: destination,
             ackMode: ackMode,
-            userDefinedHeaders: userDefinedHeaders,
+            userDefinedHeaders: subscribeHeaders,
             transactionID: self.id
         )
         let value: Value
@@ -82,13 +84,13 @@ public struct STOMPTransaction: Sendable {
         } catch {
             // call unsubscribe in unstructured Task to avoid it being cancelled
             _ = await Task {
-                try await self.connection.unsubscribe(id: id, userDefinedHeaders: userDefinedHeaders)
+                try await self.connection.unsubscribe(id: id, userDefinedHeaders: unsubscribeHeaders)
             }.result
             throw error
         }
         // call unsubscribe in unstructured Task to avoid it being cancelled
         _ = try await Task {
-            try await self.connection.unsubscribe(id: id, userDefinedHeaders: userDefinedHeaders)
+            try await self.connection.unsubscribe(id: id, userDefinedHeaders: unsubscribeHeaders)
         }.value
         return value
     }
