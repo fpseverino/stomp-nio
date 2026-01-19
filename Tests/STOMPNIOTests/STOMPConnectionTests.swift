ad073b4d1b959fbffe2fbeaac8943742ed58efd2
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2026-01-20 00:23:22 +0100

    Improve test

--- Tests/STOMPNIOTests/STOMPConnectionTests.swift
+++ Tests/STOMPNIOTests/STOMPConnectionTests.swift
@@ -504,7 +504,7 @@ struct STOMPConnectionTests {
             try await withThrowingTaskGroup { group in
                 group.addTask {
                     await #expect(throws: Never.self) {
-                        try await connection.send(frame: STOMPFrame(command: .send, headers: [.destination: "foo"]))
+                        try await connection.send(frame: STOMPFrame(command: .send, headers: [.receipt: "bar"]))
                     }
                 }
                 try await withThrowingTaskGroup { group in
@@ -513,10 +513,13 @@ struct STOMPConnectionTests {
                             try await connection.send(ByteBuffer(), to: "foo", contentType: "application/octet-stream")
                         }
                     }
-                    // wait for outbound write from both tasks
+                    // Wait for outbound write from both tasks
                     _ = try await channel.waitForOutboundWrite(as: ByteBuffer.self)
                     _ = try await channel.waitForOutboundWrite(as: ByteBuffer.self)
                     group.cancelAll()
+                    // Send RECEIPT frame to unblock the first task
+                    let receiptFrame = ByteBuffer(string: "RECEIPT\nreceipt-id:bar\n\n\u{0}")
+                    try await channel.writeInbound(receiptFrame)
                 }
             }
         }
